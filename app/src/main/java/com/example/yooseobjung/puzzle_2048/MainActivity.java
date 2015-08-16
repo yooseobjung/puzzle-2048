d88041ab43df49b4b54f8e2c33e0eb137e0ba010
YooSeob Jung <yooseob.jung17@stu.siskorea.org>
2015-08-16 15:06:49 +0900

    board size and html

--- app/src/main/java/com/example/yooseobjung/puzzle_2048/MainActivity.java
+++ app/src/main/java/com/example/yooseobjung/puzzle_2048/MainActivity.java
@@ -1,17 +1,54 @@
 package com.example.yooseobjung.puzzle_2048;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+
+    Button scoreButton, bestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Display display = this.getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        LinearLayout background = (LinearLayout) findViewById(R.id.board);
+        ViewGroup.LayoutParams params = background.getLayoutParams();
+        params.height = width;
+        params.width = width;
+        background.setLayoutParams(params);
+
+        scoreButton = (Button)findViewById(R.id.scoreboard);
+        bestButton = (Button)findViewById(R.id.bestboard);
+
+        String score = "939";
+        String scoreButtonText;
+        scoreButtonText = "<small> <font color='#FFAFA89E'> SCORE </font> </small>" + "<br/> <big> <font color='white'>" + score + "</font> </big>";
+        scoreButton.setText(Html.fromHtml(scoreButtonText));
+
+        String bestScore = "1939";
+        String bestButtonText = String.format("<small> <font color='#FFAFA89E'> BEST </font> </small><br/> <big> <font color='white'>%s</font> </big>", bestScore);
+        bestButton.setText(Html.fromHtml(bestButtonText));
+
+
     }
+
 }
