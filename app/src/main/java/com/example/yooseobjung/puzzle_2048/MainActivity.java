6a75d75ae6b7acf8e84b40a13db34848d99c4b67
ben-jung <jbehips@kaist.ac.kr>
2015-08-22 02:33:54 +0900

    fixed layout bugs

--- app/src/main/java/com/example/yooseobjung/puzzle_2048/MainActivity.java
+++ app/src/main/java/com/example/yooseobjung/puzzle_2048/MainActivity.java
@@ -6,30 +6,20 @@ import android.text.Html;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
 
-    Button scoreButton, bestButton;
+    TextView scoreTextView, bestTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        scoreButton = (Button)findViewById(R.id.scoreboard);
-        bestButton = (Button)findViewById(R.id.bestboard);
-
-        String score = "939";
-        String scoreButtonText;
-        scoreButtonText = "<small><small><small>SCORE</small></small></small>" + "<br/><big><font color='white'>" + score + "</font></big>";
-        scoreButton.setText(Html.fromHtml(scoreButtonText));
-
-        String bestScore = "1939";
-        String bestButtonText = String.format("<small><font color='#AFA89E'>BEST</font></small><br/><big><font color='red'>%s</font></big>", bestScore);
-        bestButton.setText(Html.fromHtml(bestButtonText));
-
+        scoreTextView = (TextView)findViewById(R.id.score);
+        bestTextView = (TextView)findViewById(R.id.best);
     }
 
     @Override
